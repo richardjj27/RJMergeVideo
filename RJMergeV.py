828f63851411d20dfe4159e44d6b21d1b9dc273d
Richard Johnson <richardjj627@gmail.com>
2024-02-07 07:53:04 +0000

    check folders exist

--- RJMergeV.py
+++ RJMergeV.py
@@ -1,11 +1,25 @@
 import os, subprocess, shlex
 
-BASE_DIRECTORY = "/home/rjohnson/TestMerge/"
-TARGET_DIRECTORY = "/home/rjohnson/TestMerge/Output/"
+BASE_DIRECTORY = "/mnt/multimedia/Other/~Miscellaneous/VR/09/Multipart/"
+TARGET_DIRECTORY = f"{BASE_DIRECTORY}Output/"
 TEMP_DIRECTORY = "./~Temp/"
 BASE_EXTENSION = ".mp4"
 
 if __name__ == "__main__":
+
+    os.system("clear")
+
+    if not os.path.exists(BASE_DIRECTORY):
+        print (f"{BASE_DIRECTORY} does not exist.  Terminating.")
+        exit()
+
+    if not os.path.exists(TARGET_DIRECTORY):
+        print (f"{TARGET_DIRECTORY} does not exist.  Creating.")
+        os.makedirs(TARGET_DIRECTORY, exist_ok=True)
+
+    if not os.path.exists(TEMP_DIRECTORY):
+        print (f"{TEMP_DIRECTORY} does not exist.  Creating.")
+        os.makedirs(TEMP_DIRECTORY, exist_ok=True)
 
     unique_files = set()
     files = os.listdir(BASE_DIRECTORY)
@@ -23,6 +37,8 @@ if __name__ == "__main__":
         merged_video_name = unique_file + BASE_EXTENSION
         input_file = open(TEMP_DIRECTORY + unique_file + ".lst", "w")
 
+        print (len(files))
+        exit()
         for file in files:
             if (os.path.isfile(os.path.join(BASE_DIRECTORY, file)) and \
                 file.startswith(unique_file) and \
@@ -32,7 +48,7 @@ if __name__ == "__main__":
 
         input_file.close()
 
-        ffmpeg_command = f"ffmpeg -f concat -safe 0 -i {input_file.name} -c copy {TARGET_DIRECTORY}{merged_video_name} -y"
+        #ffmpeg_command = f"ffmpeg -f concat -safe 0 -i {input_file.name} -c copy {TARGET_DIRECTORY}{merged_video_name} -y"
         print (f"*   {ffmpeg_command}")
         
         result = subprocess.run(shlex.split(ffmpeg_command), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
